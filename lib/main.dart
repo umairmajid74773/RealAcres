77da487864858ddc7227264a89d95648f1fa0715
umairmajid74773 <umairmajid74773@users.noreply.github.com>
2020-02-10 23:11:43 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class _MyAppState extends State<MyApp> {
           backgroundColor: Colors.white,
           styleTextUnderTheLoader: TextStyle(),
           loaderColor: Colors.redAccent,
+          
         ),
       ),
     );
